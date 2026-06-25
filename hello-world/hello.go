91e1cfbbe47b22fbd395663669d116b3790250d7
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2026-06-25 19:28:21 -0300

    Hello, World

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,15 +2,39 @@ package main
 
 import "fmt"
 
-const englishGreetingPrefix = "Hello, "
+const (
+	portuguese = "Portuguese"
+	spanish    = "Spanish"
+	french     = "French"
 
-func Hello(name string) string {
+	englishGreetingPrefix    = "Hello, "
+	portugueseGreetingPrefix = "Olá, "
+	spanishGreetingPrefix    = "Hola, "
+	frenchGreetingPrefix     = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
-		return englishGreetingPrefix + "World"
+		name = "World"
 	}
-	return englishGreetingPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case portuguese:
+		prefix = portugueseGreetingPrefix
+	case spanish:
+		prefix = spanishGreetingPrefix
+	case french:
+		prefix = frenchGreetingPrefix
+	default:
+		prefix = englishGreetingPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
