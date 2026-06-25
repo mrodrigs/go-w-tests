dc443a2e53d478e3189994e08d95697ffb535fc7
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2026-06-25 12:54:48 -0300

    Learn Go with tests - hello World

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishGreetingPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		return englishGreetingPrefix + "World"
+	}
+	return englishGreetingPrefix + name
 }
 
 func main() {
